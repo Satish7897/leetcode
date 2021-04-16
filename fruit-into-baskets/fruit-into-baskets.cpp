493adc7d138d922f13f1c08f5ea7150552ccc1b3
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-04-16 23:48:31 +0530

    Time: 180 ms (11.64%), Space: 64.2 MB (19.79%) - LeetHub

---
+++ fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& tr) {
+        int sIdx=0,ans=0,ct=0,n=tr.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            if(mp[tr[i]]==0)
+            {ct++;}
+            mp[tr[i]]++;
+             while(ct>2)
+              {
+                    mp[tr[sIdx]]--;
+                    if(mp[tr[sIdx]]==0)
+                        ct--;
+                    sIdx++;
+              }
+            ans=max(ans,i-sIdx+1);
+                
+            }
+            return ans;
+        }
+     
+    
+};
