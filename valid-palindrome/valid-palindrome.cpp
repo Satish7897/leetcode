fddedac93da388aa361199b40ecd1de649bc5e56
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-04-21 20:28:43 +0530

    Time: 8 ms (51.32%), Space: 7.1 MB (99.47%) - LeetHub

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -2,24 +2,29 @@ bool isAlpha(char c)
 {
     return ((c>='a'&&c<='z')||(c>='A'&&c<='Z')||(c>='0'&&c<='9'));
 }
+char convert(char c)
+{
+    if(c>='a'&&c<='z')return c;
+    return (c+32);
+}
 class Solution {
 public:
     bool isPalindrome(string s) {
         string ans;
-        for(int i=0;i<s.size();i++)
+        int i=0,j=s.size()-1;
+        while(i<j)
         {
-            if(isAlpha(s[i]))
-            {
-                if(s[i]>='A'&&s[i]<='Z')
-                    ans+=(s[i]+32);
-                else
-                    ans+=s[i];
-            }
-        }
-        int n=ans.size();
-        for(int i=0;i<n/2;i++)
-        {
-            if(ans[i]!=ans[n-i-1])return false;
+            while(i!=s.size()&&!isAlpha(s[i]))
+                i++;
+            while(j>=0&&!isAlpha(s[j]))
+                j--;
+          //  cout<<s[i]<<" "<<s[j];
+            if(i>=j)break;
+            s[i]=convert(s[i]);
+            s[j]=convert(s[j]);
+            if(s[i]!=s[j])return false;
+            i++;
+            j--;
         }
         return true;
     }
