a92a118229ee78ef48bfa8900206ffcb8588d43b
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-04-21 12:51:04 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.68%) - LeetHub

--- valid-perfect-square/valid-perfect-square.cpp
+++ valid-perfect-square/valid-perfect-square.cpp
@@ -1,7 +1,20 @@
+#define ll long long int 
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int x=sqrt(num);
-        return x*x==num;
+       ll l=1,r=num/2;
+        if(num==1)return true;
+        while(l<=r)
+        {
+            ll mid=(l+r)/2;
+            ll x=mid*mid;
+            if(x==num)
+                return true;
+            else if(x>num)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return false;
     }
 };
