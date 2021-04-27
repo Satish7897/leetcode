488a2fe0d0714acf732b8db9e7c71f7377f32e3f
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-04-28 00:23:11 +0530

    Time: 4 ms (99.65%), Space: 9.4 MB (85.39%) - LeetHub

--- car-pooling/car-pooling.cpp
+++ car-pooling/car-pooling.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>dp(1001,0);
-        for(auto u:trips)
-        {
-            dp[u[1]]+=u[0];
-            dp[u[2]]-=u[0];
-        }
-        for(int i=1;i<=1000;i++)
-           { dp[i]+=dp[i-1];if(dp[i]>capacity)return false;}
+     sort(trips.begin(),trips.end(),[](vector<int>&a,vector<int>&b){
+         if(a[1]!=b[1])return a[1]<b[1];
+         if(a[2]!=b[2])return a[2]<b[2];
+         return a[0]<b[0];
+         
+     });
+     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+     int cur=0;
+     for(int i=0;i<trips.size();i++)
+     {
+         while(pq.size()&&pq.top().first<=trips[i][1])
+         {
+             cur-=pq.top().second;
+             pq.pop();
+         }
+         cur+=trips[i][0];
+         if(cur>capacity)return false;
+         pq.push({trips[i][2],trips[i][0]});
+     }
         return true;
         
     }
