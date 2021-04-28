524b2acdeac9d4417fcaa3cf6ae081b0273e3665
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-04-28 16:03:34 +0530

    Time: 132 ms (45.63%), Space: 26.7 MB (93.53%) - LeetHub

--- longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
+++ longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
@@ -3,24 +3,33 @@ public:
     int longestBeautifulSubstring(string word) {
         int n=word.size(),ans=0,st=0,a[5]={0};
         int id[26];id[0]=0,id['e'-'a']=1,id['i'-'a']=2,id['o'-'a']=3,id['u'-'a']=4;
+        int len=0;
         for(int i=0;i<n;i++)
-        {  int idx=id[word[i]-'a'];
-            while(1)
-            { int f=0;
-               for(int i=idx+1;i<5;i++)
-               {if(a[i])f=1;}
-                if(f)
+        { 
+            int iw=id[word[i]-'a'],f=0;
+            for(int j=iw+1;j<5;j++)
+            {if(a[j])f=1;}
+            if(f==1)
+            {
+                if(iw==0)
                 {
-                    a[id[word[st]-'a']]--;
-                    st++;
+                    len=1;
+                    for(int j=iw;j<5;j++)
+                        a[j]=0;
                 }
                 else
-                    break;
+                {
+                    len=0;
+                    for(int j=0;j<5;j++)a[j]=0;
+                }
             }
+            else
+                len++;
+            a[iw]++;
+            
           //  cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4]<<"\n";
-            a[id[word[i]-'a']]++;
-            if(a[0]&&a[1]&&a[2]&&a[3]&&a[4])
-             ans=max(ans,i-st+1);
+            if(a[1]&&a[2]&&a[3]&&a[4]&&a[0])
+                ans=max(ans,len);
            
         }
       
