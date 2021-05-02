0b414967433762e056795b178d6e38017b428c8c
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-05-02 21:23:31 +0530

    Time: 20 ms (99.95%), Space: 35.9 MB (13.52%) - LeetHub

--- global-and-local-inversions/global-and-local-inversions.cpp
+++ global-and-local-inversions/global-and-local-inversions.cpp
@@ -1,13 +1,11 @@
+static int gas = [](){ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);return 0;}();
+
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int mx=nums[0];
-        for(int i=2;i<n;i++)
-        {
-            if(nums[i]<mx)return false;
-            mx=max(mx,nums[i-1]);
-        }
-        return true;
+    bool isIdealPermutation(vector<int>& a) {
+        bool ret = true;
+        for(int i = a.size(); (--i>=0) & ret;)
+            ret = abs(a[i] - i) < 2;
+        return ret;
     }
 };
