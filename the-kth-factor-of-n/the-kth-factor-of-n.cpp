7804215a9e5b614e9fe23c1b7b4c9233cdd28049
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-05-12 09:10:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.29%) - LeetHub

---
+++ the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int>v;
+        for(int i=1;i<sqrt(n);i++)
+            if(n%i==0&&--k==0)
+                return i;
+        for(int i=sqrt(n);i>=1;i--)
+            if(n%(n/i)==0&&--k==0)
+                return n/i;
+        return -1;
+    }
+};
