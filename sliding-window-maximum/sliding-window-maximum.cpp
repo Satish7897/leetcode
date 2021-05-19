e3d3a64f7369ef653bccdfb982a2379d44f3729b
SATISH SHRIWAS <55326969+Satish7897@users.noreply.github.com>
2021-05-19 09:48:36 +0530

    Time: 264 ms (39.58%), Space: 131.8 MB (72.33%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.cpp
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
+       deque<int>q;
         vector<int>ans;
-        priority_queue<int>pq;
-        int st=0;
-        for(int i=0;i<k-1;i++)
-        {
-            mp[nums[i]]++;
-            pq.push(nums[i]);
-        }
-        for(int i=k-1;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-            pq.push(nums[i]);
-
-            while(mp[pq.top()]==0)
-              pq.pop();
-            ans.push_back(pq.top());
-            mp[nums[st++]]--;
-        }
+       for(int i=0;i<nums.size();i++)
+       {
+           while(q.size()&&i-q.front()>=k)
+               q.pop_front();
+           while(q.size()&&nums[i]>=nums[q.back()])
+               q.pop_back();
+           q.push_back(i);
+           if(i>=k-1)
+           ans.push_back(nums[q.front()]);
+       }
         return ans;
-
     }
 };
